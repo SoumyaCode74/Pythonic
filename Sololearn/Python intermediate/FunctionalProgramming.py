e486a9945ca452f76cfaa229275c7a468b69791e
SoumyaCode74 <100553212+SoumyaCode74@users.noreply.github.com>
2022-06-05 13:16:53 +0530

    Update FunctionalProgramming.py

--- Sololearn/Python intermediate/FunctionalProgramming.py
+++ Sololearn/Python intermediate/FunctionalProgramming.py
@@ -77,7 +77,13 @@ def countdown():
     while i > 0:
         yield i
         i -= 1
-
 for i in countdown():
     print(i)
+print(dashes)
+def numbers(x):
+    for i in range(x):
+        if i % 2 == 0:
+            yield i
+
+print(list(numbers(11)))
 
